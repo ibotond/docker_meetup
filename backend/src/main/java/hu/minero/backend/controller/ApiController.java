fa739486cee0cb25eeeb7f1f0d478c059c69b6cf
illbot <botond.illes@z-consulting.eu>
2022-08-04 19:43:50 +0200

    Basic backend

--- backend/src/main/java/hu/minero/backend/controller/ApiController.java
+++ backend/src/main/java/hu/minero/backend/controller/ApiController.java
@@ -1,2 +1,13 @@
-package hu.minero.backend.controller;public class ApiController {
+package hu.minero.backend.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping("/hello")
+    String helloWorld(){
+        return "Hello World!";
+    }
 }
